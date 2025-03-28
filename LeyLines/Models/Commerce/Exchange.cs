e4e7872892025d5d1878295a4ec8a6cde6c64c1f
Gustavo Diaz Galeas <diazg.gustavoa@gmail.com>
2025-03-28 17:15:07 -0400

    Initial batch of /v2/commerce/ datamodels

---
+++ LeyLines/Models/Commerce/Exchange.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) 2025 Gustavo Diaz Galeas
+// This file is part of LeyLines.NET SDK.
+//
+// Licensed under the GNU AGPL v3.0 (see LICENSE file).
+// Additional restriction: This software may not be used for commercial purposes
+// without prior written permission from the author.
+
+using System.Text.Json.Serialization;
+
+namespace LeyLines.Models.Commerce
+{
+    public class ExchangeRate
+    {
+        [JsonPropertyName("coins_per_gem")]
+        public int CoinsPerGem { get; set; }
+
+        [JsonPropertyName("quantity")]
+        public int Quantity {  get; set; }
+    }
+}
---
+++ LeyLines/Models/Commerce/Prices.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) 2025 Gustavo Diaz Galeas
+// This file is part of LeyLines.NET SDK.
+//
+// Licensed under the GNU AGPL v3.0 (see LICENSE file).
+// Additional restriction: This software may not be used for commercial purposes
+// without prior written permission from the author.
+
+using System.Text.Json.Serialization;
+
+namespace LeyLines.Models.Commerce
+{
+    public class PriceEntry
+    {
+        [JsonPropertyName("quantity")]
+        public int Quantity { get; set; }
+
+        [JsonPropertyName("unit_price")]
+        public int UnitPrice { get; set; }
+    }
+
+    public class Prices
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("buys")]
+        public required PriceEntry Buys { get; set; }
+
+        [JsonPropertyName("sells")]
+        public required PriceEntry Sells { get; set; }
+    }
+}
